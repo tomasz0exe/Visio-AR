8cd4f65c859f74c265234a91c060dcc2f6b68fc8
tomasz0exe <david_wolynski@outlook.com>
2024-11-20 14:31:22 +0100

    added support for Meta Quest 3S

--- Assets/Visio AR/Scripts/UIToggle.cs
+++ Assets/Visio AR/Scripts/UIToggle.cs
@@ -8,7 +8,7 @@ public class UIToggle : MonoBehaviour
     private void Update()
     {
         // Check if the Y button on the left controller is pressed
-        if (OVRInput.GetDown(OVRInput.Button.Three))
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
         {
             // Toggle the active state of the target object
             if (targetObject != null)
